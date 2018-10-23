da7de82c876d420e0c1c0a9cf34529c0c30a6005
lingling82902@gmail.com <lingling82902@gmail.com>
2018-10-22 20:38:33 -0700

    Answeres a few of the questions

--- exericse.R
+++ exericse.R
@@ -2,30 +2,35 @@
 # install.packages("dplyr")
 library(dplyr)
 
-# Read in the NBA team data of the 2016-2017 season from the data directory  
+# Read in the NBA team data of the 2016-2017 season from the data directory
 # into a variable called `team.data` using `read.csv`
+
 team.data <- read.csv("data/teams.csv", stringsAsFactors = FALSE)
 
-
-# The data.frame team.data should now be accessible to you. 
-# View it, and get some basic information about the number of rows/columns. 
-# Note the "X" preceding some of the column titles as well as the "*" 
+# The data.frame team.data should now be accessible to you.
+# View it, and get some basic information about the number of rows/columns.
+# Note the "X" preceding some of the column titles as well as the "*"
 # following the names of teams that made it to the playoffs that year.
-View(team.data)
 
+View(team.data)
 
 # Add a column that gives the turnovers to steals ratio (TOV / STL) for each team
 
+team.data <- mutate(team.data, turns_steals_ratio= TOV / STL)
 
 # Sort the teams from lowest turnover/steal ratio to highest
 
+-- use an arrange function!
 
 #Find the average BLK and STL for teams having a TOV greater than the average TOV of all teams
 
+-- 2 parts, find the AVG BLK And STL teams
+-- First filter, then find AVG
 
-# Get the team that had the highest Total Rebounds (TRB) only with the columns 
+# Get the team that had the highest Total Rebounds (TRB) only with the columns
 # Team and TRB  *using one line of code*
 
+-- order from highest to lowert and and get the name on top and return!
 
 # Print only the name of the team that had the highest total rebounds
 # (that also happens to be the greatest team of all time)
@@ -33,23 +38,33 @@ View(team.data)
 
 ## Let's change gears!
 
-# Read in the Pokemon data from the data directory  
+# Read in the Pokemon data from the data directory
 # into a variable called `pokemon` using `read.csv`. Remember to not read strings in as factors.
 
-# First, View() the data set to see what info you have to work with 
+# First, View() the data set to see what info you have to work with
 
 
 # Find all the Pokemon that are "Water" or "Ghost" Type 1 and have a speed higher than 50
 
+-- Filter type 1 and see if water or ghost and also check if > 50 and set to new variable
+
 # Find the average HP, median HP, min HP and max HP for each of the type of generations
 
-#FIND THE NUMBER OF POKEMONS THAT ARE LEGENDARY AND THE NUMBER OF POKEMONS THAT ARE NOT LEGENDARY 
+-- Group the tables in terms to generations (GROUP BY),
+-- USER summarize function to find avg. min, max
+
+#FIND THE NUMBER OF POKEMONS THAT ARE LEGENDARY AND THE NUMBER OF POKEMONS THAT ARE NOT LEGENDARY
 #THAT HAVE A HIGHER ATTACK THAN DEFENSE VALUE
 #NOTE: IT SHOULD BE DONE IN ONE RUN OF DPLYR
 #HINT : Look into count() function by dplyr
 
+-- Filter if they are legendary, then mutate finding if attack is greater than their defense
+
 #Find the generation that has the most number of pokemons with Type.1 as "Fire"
+
+-- grouping by generations, filter based on type 1
 
 # WHICH Type 1 pokemon has the overall within group max value of SUM of HP, Attack, Defense, Sp..Att, Sp.Def, Speed
 #HINT : LOOK AT THE DATASET! - THERE IS A SUPER EASY WAY TO DO THIS.
 
+-- Group by for type 1! then summarize seeing sum of children, then filter to find max, and select!
